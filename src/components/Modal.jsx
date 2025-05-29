f0dbe0ff91e1f2da2ffef226bddd771758775a62
Keerthan <Deverakeerthan3030@gmail.com>
2025-05-29 19:43:54 +0530

    Adding the Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,22 @@
 import { createPortal } from "react-dom"
-export default function Modal({ children, onClose }) {
-    return(
-        <dialog>
+import { forwardRef,useImperativeHandle, useRef} from "react"
+const Modal = forwardRef(function Modal({ children,Caption }, ref) {
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => ({
+        open(){
+            dialog.current.showModal();
+        }
+    }));
+    return createPortal(
+        <dialog ref={dialog} className="w-[35rem] backdrop-brightness-100  p-4 bg-stone-800 text-stone-50 rounded-md">
             {children}
-        </dialog>
+            <form>
+                <button className="absolute  top-2 right-2 text-stone-400 hover:text-stone-100">
+                {Caption}
+            </button>
+            </form>
+        </dialog>,document.getElementById("modal-root")
     )
-}
+})
+export default Modal;
--- src/components/Newproject.jsx
+++ src/components/Newproject.jsx
@@ -1,6 +1,10 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 export default function Newproject({onAddProject}){
+
+    const modalRef = useRef();
+
     const title = useRef();
     const description = useRef();
     const DueDate = useRef();
@@ -12,7 +16,7 @@ export default function Newproject({onAddProject}){
 
         //Validation ..
         if(enteredTitle.trim() === "" || enteredDescription.trim() === "" || enteredDueDate.trim() === "") {
-            alert("Please fill in all fields.");
+            modalRef.current.open();
             return;
         }
        
@@ -23,26 +27,31 @@ export default function Newproject({onAddProject}){
         });
     }
     return(
+        <>
+            <Modal ref={modalRef} Caption="Close">
+                <h2 className="text-lg font-bold text-stone-50">Please fill all the fields</h2>
+                <p className="text-sm text-stone-300">All fields are required to create a new project.</p>
+            </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex gap-4 justify-end items-center my-4"> 
+                    <li>
+                        <button 
+                        className="px-9 bg-stone-800 text-stone-50 rounded-md hover:text-stone-300"
+                        onClick={handleSave}
+                        > Save </button>
+                    </li> 
+                    <li>
+                        <button className="bg-stone-800 text-stone-50 rounded-md px-6 text hover:text-stone-300"> Cancel </button>
+                    </li>
+                </menu>
+                
 
-        <div className="w-[35rem] mt-16">
-            <menu className="flex gap-4 justify-end items-center my-4"> 
-                <li>
-                    <button 
-                    className="px-9 bg-stone-800 text-stone-50 rounded-md hover:text-stone-300"
-                    onClick={handleSave}
-                    > Save </button>
-                </li> 
-                <li>
-                    <button className="bg-stone-800 text-stone-50 rounded-md px-6 text hover:text-stone-300"> Cancel </button>
-                </li>
-            </menu>
-            
-
-            <div>
-                <Input type="text" ref={title} label="Title"/>
-                <Input ref={description} label="Description" textarea/>
-                <Input type="date" ref={DueDate} label="Date" />
+                <div>
+                    <Input type="text" ref={title} label="Title"/>
+                    <Input ref={description} label="Description" textarea/>
+                    <Input type="date" ref={DueDate} label="Date" />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
